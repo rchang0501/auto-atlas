212fdb4d494570bd4a6a4a278ee7d7141b8fea95
Ryan Chang <ryanchang0501@gmail.com>
2022-04-26 13:13:36 -0400

    imported Tailwind CSS classnames and resolved reducers bug

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,7 @@
 import { View, Text, ImageBackground, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
 
 import { assets, FONTS, COLORS, SIZES } from "../constants";
 
